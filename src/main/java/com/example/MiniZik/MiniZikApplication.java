560a7c37e2cb21871a80e2f9ff0ae1dcb23a0ead
PIGNA Carole <carole.pigna@gmail.com>
2024-09-23 11:38:06 +0200

    initialisation spring boot

---
+++ src/main/java/com/example/MiniZik/MiniZikApplication.java
@@ -0,0 +1,13 @@
+package com.example.MiniZik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniZikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniZikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MiniZik/MiniZikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiniZik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniZikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
